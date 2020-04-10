b4f89d42f521787eba50d9f7a68028d46e7d55cf
pavel <browser091@gmail.com>
2020-04-10 16:14:06 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import moment from "moment";
 
-function App() {
+const DATA = {
+  timer: 1234567,
+  laps: [1234, 2345, 34567, 98765, 898998908],
+};
+
+function Timer({ interval }) {
+  const duration = moment.duration(interval);
+  const centiseconds = Math.floor(duration.milliseconds() / 10);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <span className="duration">
+      {duration.minutes()}:{duration.seconds()},{centiseconds}
+    </span>
   );
 }
 
+function RoundButton({ value, color, background }) {
+  return (
+    <input
+      type="button"
+      value={value}
+      style={{ color: color, background: background }}
+    />
+  );
+}
+
+function Lap({ number, interval }) {
+  return (
+    <div className="lap">
+      <span>Lap {number}</span>
+      <span>{interval}</span>
+    </div>
+  );
+}
+// debugger;
+function LapsTable({ laps }) {
+  const duration = moment.duration(laps);
+  const centiseconds = Math.floor(duration.milliseconds() / 10);
+  return (
+    <ul className="lapsTable">
+      {laps.map((lap, index) => (
+        <li className="laps" key={laps.length - index}>
+          <span>Lap {laps.length - index}</span>
+          <span>
+            {duration.minutes()}:{duration.seconds()},{centiseconds}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+function Lapss({ interval }) {
+  const duration = moment.duration(interval);
+  const centiseconds = Math.floor(duration.milliseconds() / 10);
+  return (
+    <span>
+      {duration.minutes()}:{duration.seconds()},{centiseconds}
+    </span>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Timer interval={DATA.timer} />
+      <div className="buttonRow">
+        <RoundButton value="Reset" color="#ffffff" background="#3d3d3d" />
+        <RoundButton value="Start" color="#50d167" background="#1b361f" />
+      </div>
+      <LapsTable laps={DATA.laps} />
+      <Lapss interval={DATA.laps} />
+    </>
+  );
+}
+console.log(DATA.laps);
 export default App;
+// color="#50d167"
